f37036669189f8df9a6463714eda023296319a44
Mart H <pietertjepost123@gmail.com>
2022-08-24 21:07:27 +0200

    Opdracht 2.1; Beveiliging update in de controller, vooral database input valideren + comments

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Product;
 use App\User;
+use Illuminate\Auth\Events\Validated;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
@@ -19,23 +20,45 @@ class ProductController extends Controller
 
     public function new(Request $request)
     {
+
+        // valideer request
         $validator = Validator::make($request->all(), [
-            'name' => 'unique:products',
+            'name' => 'required|unique:products|max:255',
         ]);
  
+        // Wanneer de validatie faalt return een message
         if ($validator->fails()) {
             return redirect('/products')->with('statusBad', 'Name was already used!');
         }
         
-        
-        DB::insert("INSERT INTO products (name) VALUES ('".$request->name."')");
+
+        // haal de gevalideerde data op
+        $validated = $validator->validated();
+
+        DB::insert("INSERT INTO products (name) VALUES ('".$validated['name']."')");
 
         return redirect('/products')->with('status', 'Product saved');
     }
 
     public function delete(Request $request)
     {
-        DB::delete("DELETE FROM products WHERE id = ".$request->id);
+
+         // valideer request
+         $validator = Validator::make($request->all(), [
+            'id' => 'integer',
+        ]);
+ 
+        // Wanneer de validatie faalt return een message
+        if ($validator->fails()) {
+            return redirect('/products')->with('statusBad', 'Er ging iets mis!');
+        }
+        
+
+        // haal de gevalideerde data op
+        $validated = $validator->validated();
+
+
+        DB::delete("DELETE FROM products WHERE id = ".$validated['id']);
 
         return redirect('/products')->with('status', 'Product was deleted');
     }
